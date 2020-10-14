12df34bcebd688b0d80402b9177e7324f3b67356
mainlyetcetera <ericcampbell294@gmail.com>
2020-10-13 23:18:54 -0600

    Pass full after eating 3 times test

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -4,7 +4,14 @@ class Dragon {
     this.rider = rider;
     this.color = color;
     this.hungry = true;
+    this.timesEaten = 0;
   }
+
+  eat() {
+    this.timesEaten++;
+    this.timesEaten >= 3 ? this.hungry = false : this.hungry;
+  }
+
 }
 
 module.exports = Dragon;
