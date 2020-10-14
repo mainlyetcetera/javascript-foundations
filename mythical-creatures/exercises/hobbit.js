45c1e3c4fc0f38e801b85abf0eb73be84679069c
mainlyetcetera <ericcampbell294@gmail.com>
2020-10-13 23:46:24 -0600

    Pass all age tests

--- mythical-creatures/exercises/hobbit.js
+++ mythical-creatures/exercises/hobbit.js
@@ -2,10 +2,14 @@ class Hobbit {
   constructor (name) {
     this.name = name;
     this.age = 0;
+    this.adult = false;
+    this.old = false;
   }
 
   celebrateBirthday() {
     this.age++;
+    this.age > 32 ? this.adult = true : this.adult;
+    this.age >= 101 ? this.old = true : this.old;
   }
 
 }
