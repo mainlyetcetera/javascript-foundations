76b2f4b09947ab29be7de809e51ff0004af59a90
mainlyetcetera <ericcampbell294@gmail.com>
2020-10-17 19:44:26 -0600

    Pass hat tests

--- mythical-creatures/exercises/magician.js
+++ mythical-creatures/exercises/magician.js
@@ -1,6 +1,7 @@
 class Magician {
   constructor(magician) {
-    !magician ? this.topHat = true
+    // magician.topHat ? this.topHat = magician.topHat : this.topHat = true;
+    !magician || magician.topHat ? this.topHat = true
       : magician.topHat === undefined ? (this.name = magician.name, this.topHat = true)
       : (this.name = magician.name, this.topHat = magician.topHat)
   }
