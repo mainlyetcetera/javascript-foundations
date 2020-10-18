e946276688453fb6e2438e832386b2016d073b2c
mainlyetcetera <ericcampbell294@gmail.com>
2020-10-17 21:38:52 -0600

    Pass let first victim go when stoning fourth test

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -5,8 +5,11 @@ class Medusa {
   }
 
   stare(victim) {
-    this.statues.length <= 2 ? this.statues.push(victim) : this.statues;
-    victim.stoned = true;
+    this.statues.length === 3 ? (this.statues[0].stoned = false, this.statues.shift(), this.statues.push(victim), victim.stoned = true)
+      : this.statues.length <= 2 ? (this.statues.push(victim), victim.stoned = true)
+      : this.statues
+    // this.statues.push(victim) : this.statues;
+    // victim.stoned = true;
   }
 
 }
